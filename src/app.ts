1df5ca9306a04667a0c94aa24b7aa765dad18059
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-09-20 17:31:23 +0200

    Final version of PoC

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,34 @@
-import express from 'express';
-import axios from 'axios';
-import FormData from 'form-data';
+import express from "express";
+import axios from "axios";
+import FormData from "form-data";
 import fs from "fs";
 
-const SLACK_SIGNING_SECRET = "";
-const SLACK_CLIENT_ID = "";
-const SLACK_CLIENT_SECRET = "";
+const SLACK_SIGNING_SECRET = "acf31a7376b19c7d55fafabfd3bed248";
+const SLACK_CLIENT_ID = "12922529104.5894776759175";
+const SLACK_CLIENT_SECRET = "cf00d1d6730ca5b040d79309530ecef6";
 
-const SRCDIR = "src/"
+const SRCDIR = "src/";
 
 let access_token = "";
 
 const app = express();
 const port = 3001;
 
-save_authed_user({test: "value"})
-
 // Middleware to parse JSON body
 app.use(express.json());
 
-// OAuth Callback URL
+// OAuth Callback URL has to be the same as in the Slack Application Panel under OAuth page
 // const redirectUri = "http://localhost:3001/slack/auth/callback";
-const redirectUri = "https://long-ants-melt.loca.lt/slack/auth/callback";
+const redirectUri = "https://real-hands-tickle.loca.lt/slack/auth/callback";
 
 // OAuth Step 1: Redirect users to Slack's authorization URL
-app.get('/slack/auth', (req, res) => {
+app.get("/slack/auth", (req, res) => {
     const authUrl = `https://slack.com/oauth/v2/authorize?client_id=${SLACK_CLIENT_ID}&&scope=&user_scope=channels%3Ahistory%2Cchannels%3Aread%2Cchat%3Awrite%2Cgroups%3Aread%2Cim%3Ahistory%2Cim%3Aread%2Cim%3Awrite%2Cmpim%3Ahistory%2Cmpim%3Aread%2Cusers.profile%3Aread%2Cusers.profile%3Awrite&redirect_uri=${redirectUri}`;
     res.redirect(authUrl);
 });
 
 // OAuth Step 2: Handle the OAuth callback
-app.get('/slack/auth/callback', async (req, res) => {
+app.get("/slack/auth/callback", async (req, res) => {
     const { code } = req.query;
     try {
         const formData = new FormData();
@@ -40,31 +38,37 @@ app.get('/slack/auth/callback', async (req, res) => {
 
         const config = {
             headers: {
-                'Content-Type': 'multipart/form-data',
+                "Content-Type": "multipart/form-data",
             },
         };
-        const response = await axios.post('https://slack.com/api/oauth.v2.access', formData, config);
+        const response = await axios.post(
+            "https://slack.com/api/oauth.v2.access",
+            formData,
+            config
+        );
 
-        console.log(response)
+        console.log(response);
 
         access_token = response.data.authed_user.access_token;
 
         // Now you have the user's access token
-        console.log('User Access Token:', access_token);
+        console.log("User Access Token:", access_token);
 
         // You can save it for later use, typically in a database
         // or associate it with the user's account
 
-        res.send('OAuth process completed successfully!');
+        res.send("OAuth process completed successfully!");
     } catch (error) {
-        console.error('OAuth error:', error);
-        res.status(500).send('OAuth process failed.');
+        console.error("OAuth error:", error);
+        res.status(500).send("OAuth process failed.");
     }
 });
 
-// Test endpoint
-app.get('/slack/auth/test', async (req, res) => {
+// Message endpoint
+app.get("/slack/auth/send-message", async (req, res) => {
     const { mess, conversations } = req.query;
+    console.log(mess, conversations);
+    console.log(access_token);
 
     try {
         const data = {
@@ -77,20 +81,21 @@ app.get('/slack/auth/test', async (req, res) => {
             },
         };
 
-        const response = await axios.post('https://slack.com/api/chat.postMessage', data, config)
+        const response = await axios.post(
+            "https://slack.com/api/chat.postMessage",
+            data,
+            config
+        );
 
         // Now you have the code received from Slack
-        console.log('TEST: ', response);
-
         // You can save it for later use, typically in a database
         // or associate it with the user's account
 
-        res.send(`Test successful. \n ${response}`);
+        res.send(`Message successful. \n ${response}`);
     } catch (error) {
-        console.error('Sending Message error:', error);
+        console.error("Sending Message error:", error);
         res.status(500).send(`Process of Sending Message failed. \n ${error}`);
     }
-
 });
 
 // Start the Express server
@@ -100,7 +105,7 @@ app.listen(port, () => {
 
 function save_authed_user(obj: Object): void {
     const json = JSON.stringify(obj);
-    fs.writeFile(SRCDIR+"authed_user.json", json, "utf8", (err) => {
+    fs.writeFile(SRCDIR + "authed_user.json", json, "utf8", (err) => {
         if (err) throw err;
     });
 }
