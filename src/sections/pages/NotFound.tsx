5968a4d8f61337dc6859e2dbbb7d7adc9dd7ad31
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-09-26 15:04:31 +0200

    not found redirect

--- src/sections/pages/NotFound.tsx
+++ src/sections/pages/NotFound.tsx
@@ -1,9 +1,15 @@
-function NotFound() {
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-    return (
-          <h1>Not Found</h1>
-    );
-  }
-  
-  export default NotFound;
-  
+function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  }, []);
+  return <h1>Not Found</h1>;
+}
+
+export default NotFound;
