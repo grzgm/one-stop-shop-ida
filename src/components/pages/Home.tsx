ecb58ed07b1a2491a39496d0aec79fcfc956d5ec
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-10-05 14:19:25 +0200

    Home page done

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,15 @@
 import CardContainer from "../tiles/CardContainer";
+import "../../css/components/pages/home.css"
 
 function Home() {
-
   return (
     <div className="content">
       <div className="description">
-        <h1>Welcome to iDA One Stop Shop</h1>
-        <h2>Place where you have everything you need!</h2>
+        <h1>Welcome to</h1>
+        <h1>iDA</h1>
+        <h1>One Stop Shop</h1>
+        <p>Place where you</p>
+        <p>have everything you need!</p>
       </div>
       <CardContainer />
       <CardContainer />
