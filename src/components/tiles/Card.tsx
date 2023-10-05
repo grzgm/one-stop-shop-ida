2198c2cc7d8ff1e4035f49a25d37195d6bffaa6b
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-10-05 14:41:39 +0200

    paragraph tag added

--- src/components/tiles/Card.tsx
+++ src/components/tiles/Card.tsx
@@ -5,12 +5,12 @@ import "../../css/components/tiles/card.css";
 function Card() {
   return (
     <div className="card">
-      <div className="card__text body--small">
+      <div className="card__text">
         <div className="card__text__title">
           <img className="card__text__icon" src={personHappy} alt="iDA" />
           <h1>Title</h1>
         </div>
-        Description
+        <p>Description</p>
       </div>
       <div className="card__arrow">
         <KeyboardArrowRightIcon fontSize="inherit" />
