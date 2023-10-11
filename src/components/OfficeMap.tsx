f5963333954b26c9f749fbfbd107f76bcf0a6aae
Grzegorz <g.malisz@student.fontys.nl>
2023-10-11 18:55:56 +0200

    redirect, change office by clicking on google map

--- src/components/OfficeMap.tsx
+++ src/components/OfficeMap.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
 import { officeInformationData } from '../assets/OfficeInformationData';
 
 const mapStyles = {
@@ -11,24 +11,27 @@ interface OfficeMapProps {
 }
 
 function OfficeMap({ switchOffice }: OfficeMapProps) {
-    return (
-        <LoadScript googleMapsApiKey="AIzaSyDSE5J50qb82mBZfTGZheGJ0Jg8_vnk0_o">
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyDSE5J50qb82mBZfTGZheGJ0Jg8_vnk0_o',
+    });
+
+    if (isLoaded) {
+        return (
             <GoogleMap
                 mapContainerStyle={mapStyles}
                 center={officeInformationData['Utrecht'].officeInformation.coords}
-                zoom={7}
-            >
+                zoom={7}>
                 {Object.values(officeInformationData).map((office) => (
                     <MarkerF
                         key={office.officeName}
                         position={office.officeInformation.coords}
-                        title={office.officeName} 
-                        onClick={()=>switchOffice(office.officeName)}
-                        />
+                        title={office.officeName}
+                        onClick={() => switchOffice(office.officeName)} />
                 ))}
             </GoogleMap>
-        </LoadScript>
-    );
+        );
+    }
+    return (<h1>Google Maps are loading...</h1>);
 }
 
 export default OfficeMap;
--- src/components/pages/offices/Offices.tsx
+++ src/components/pages/offices/Offices.tsx
@@ -6,14 +6,14 @@ import OfficeMap from "../../OfficeMap";
 import { useNavigate } from "react-router-dom";
 
 function Offices() {
-  const {currentOffice, setCurrentOffice} = useContext(CurrentOfficeContext);
+  const { currentOffice, setCurrentOffice } = useContext(CurrentOfficeContext);
   const navigate = useNavigate();
 
   const SwitchOffice = (officeName: string) => {
     setCurrentOffice(officeName);
     navigate("/");
   }
-  
+
   return (
     <div className="content">
       <div className="description">
@@ -22,15 +22,15 @@ function Offices() {
         <BodyNormal>or tap on the office!</BodyNormal>
       </div>
       <div>
-        <OfficeMap switchOffice={SwitchOffice}/>
+        <OfficeMap switchOffice={SwitchOffice} />
       </div>
       <div className="content__panels">
-        <Panel linkAddress="/office-details" title="Utrecht" description="Orteliuslaan 25 3528BA" onClick={()=>setCurrentOffice("Utrecht")}/>
-        <Panel linkAddress="/office-details" title="Amsterdam" description="Cruquiusweg 110F 1019AK" onClick={()=>setCurrentOffice("Amsterdam")}/>
-        <Panel linkAddress="/office-details" title="Eindhoven" description="High Tech Campus 69 5656AE" onClick={()=>setCurrentOffice("Eindhoven")}/>
-        <Panel linkAddress="/office-details" title="Kontich" description="Prins boudewijnlaan 24e 2550" onClick={()=>setCurrentOffice("Kontich")}/>
-        <Panel linkAddress="/office-details" title="Hasselt" description="Kempische Steenweg 311 3500" onClick={()=>setCurrentOffice("Hasselt")}/>
-        <Panel linkAddress="/office-details" title="Merelbeke" description="Guldensporenpark 88 9820" onClick={()=>setCurrentOffice("Merelbeke")}/>
+        <Panel linkAddress="/office-details" title="Utrecht" description="Orteliuslaan 25 3528BA" onClick={() => setCurrentOffice("Utrecht")} />
+        <Panel linkAddress="/office-details" title="Amsterdam" description="Cruquiusweg 110F 1019AK" onClick={() => setCurrentOffice("Amsterdam")} />
+        <Panel linkAddress="/office-details" title="Eindhoven" description="High Tech Campus 69 5656AE" onClick={() => setCurrentOffice("Eindhoven")} />
+        <Panel linkAddress="/office-details" title="Kontich" description="Prins boudewijnlaan 24e 2550" onClick={() => setCurrentOffice("Kontich")} />
+        <Panel linkAddress="/office-details" title="Hasselt" description="Kempische Steenweg 311 3500" onClick={() => setCurrentOffice("Hasselt")} />
+        <Panel linkAddress="/office-details" title="Merelbeke" description="Guldensporenpark 88 9820" onClick={() => setCurrentOffice("Merelbeke")} />
       </div>
     </div>
   );
