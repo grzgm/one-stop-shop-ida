df67c7e6b5b95e858c6c4494eb8c92631ff8df22
Grzegorz <g.malisz@student.fontys.nl>
2023-10-16 11:34:39 +0200

    cards on home page according to chosen office

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,20 +1,25 @@
 import CardContainer from "../tiles/CardContainer";
 import "../../css/components/pages/home.css";
 import { BodyNormal, HeadingLarge } from "../text-wrapers/TextWrapers";
-
-
-const officeDetails = [{linkAddress:"/office-details/reserve-desk", title:"Reserve a Desk", description:"Reserve a Desk"},
-{linkAddress:"/office-details/lunch", title:"Lunch", description:"Sign up for Lunch"},
-{linkAddress:"/office-details/office-information", title:"Office Information", description:"Office Information Access Parking Wi-Fi How many ppl in the office"},
-{linkAddress:"/office-details/presence", title:"Presence", description:"Let your colleague know when you are present"}]
-
-const employeePortal = [
-{linkAddress: "https://werknemer.loket.nl/#/login?returnUrl=start", title: "External", description: "Werknemerloket Web Page"},
-{linkAddress: "/employee-portal/sick-leave", title: "Sick Leave", description: "Register your absence"},
-{linkAddress: "/employee-portal/vacation", title: "Vacation", description: "Plan your off days and see their balance"},
-{linkAddress: "/employee-portal/scheduling", title: "Scheduling", description: "Plan out your wor schedule"}]
+import { useContext } from "react";
+import CurrentOfficeContext from "../../contexts/CurrentOfficeContext";
+import { officeInformationData } from "../../assets/OfficeInformationData";
 
 function Home() {
+  const officeName = useContext(CurrentOfficeContext).currentOffice;
+  const currentOfficeInformationData = officeInformationData[officeName]
+
+  const officeDetails = [(currentOfficeInformationData.canReserveDesk ? {linkAddress:"/office-details/reserve-desk", title:"Reserve a Desk", description:"Reserve a Desk"} : {}),
+  (currentOfficeInformationData.canRegisterLunch ? {linkAddress:"/office-details/lunch", title:"Lunch", description:"Sign up for Lunch"} : {}),
+  {linkAddress:"/office-details/office-information", title:"Office Information", description:"Office Information Access Parking Wi-Fi How many ppl in the office"},
+  (currentOfficeInformationData.canRegisterPresence ? {linkAddress:"/office-details/presence", title:"Presence", description:"Let your colleague know when you are present"} : {})].filter(item => Object.keys(item).length !== 0);
+  
+  const employeePortal = [
+  {linkAddress: "https://werknemer.loket.nl/#/login?returnUrl=start", title: "External", description: "Werknemerloket Web Page"},
+  {linkAddress: "/employee-portal/sick-leave", title: "Sick Leave", description: "Register your absence"},
+  {linkAddress: "/employee-portal/vacation", title: "Vacation", description: "Plan your off days and see their balance"},
+  {linkAddress: "/employee-portal/scheduling", title: "Scheduling", description: "Plan out your wor schedule"}]
+
   return (
     <div className="content">
       <div className="description">
@@ -25,7 +30,7 @@ function Home() {
         <BodyNormal>have everything you need!</BodyNormal>
       </div>
       <CardContainer title="EmployeePortal" cardProps={employeePortal}/>
-      <CardContainer title="Office Details" cardProps={officeDetails}/>
+      <CardContainer title={`Office Details ${officeName}`} cardProps={officeDetails}/>
     </div>
   );
 }
