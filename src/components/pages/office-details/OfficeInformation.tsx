f5ad02857140ed8dacc88a509687482a97b9764a
Grzegorz <g.malisz@student.fontys.nl>
2023-10-16 12:41:25 +0200

    office information page contetn

--- src/components/pages/office-details/OfficeInformation.tsx
+++ src/components/pages/office-details/OfficeInformation.tsx
@@ -1,9 +1,43 @@
-function OfficeInformation() {
+import { useContext } from "react";
+import CurrentOfficeContext from "../../../contexts/CurrentOfficeContext";
+import { officeInformationData } from "../../../assets/OfficeInformationData";
+import { BodyNormal, HeadingLarge, HeadingSmall } from "../../text-wrapers/TextWrapers";
+import "../../../css/components/pages/office-details/office-information.css"
 
-    return (
-          <h1>Office Information</h1>
-    );
-  }
-  
-  export default OfficeInformation;
-  
+function OfficeInformation() {
+	const officeName = useContext(CurrentOfficeContext).currentOffice;
+	const currentOfficeInformationData = officeInformationData[officeName]
+
+	return (
+		<div className="content">
+			<div className="description">
+				<HeadingLarge>{`${officeName} Office Information`}</HeadingLarge>
+				<BodyNormal>All information you need!</BodyNormal>
+			</ div>
+			<main id="office-information-main">
+				<section>
+					<HeadingSmall>Address</HeadingSmall>
+					<BodyNormal>{currentOfficeInformationData.officeInformation.address}</BodyNormal>
+				</section>
+				<section>
+					<HeadingSmall>Opening Hours</HeadingSmall>
+					<BodyNormal>{currentOfficeInformationData.officeInformation.openingHours}</BodyNormal>
+				</section>
+				<section>
+					<HeadingSmall>Access Information</HeadingSmall>
+					<BodyNormal>{currentOfficeInformationData.officeInformation.accessInformation}</BodyNormal>
+				</section>
+				<section>
+					<HeadingSmall>Parking Information</HeadingSmall>
+					<BodyNormal>{currentOfficeInformationData.officeInformation.parkingInformation}</BodyNormal>
+				</section>
+				{currentOfficeInformationData.canRegisterLunch && <section>
+					<HeadingSmall>Lunch Information</HeadingSmall>
+					<BodyNormal>{currentOfficeInformationData.officeInformation.lunchInformation}</BodyNormal>
+				</section>}
+			</main>
+		</ div>
+	);
+}
+
+export default OfficeInformation;
