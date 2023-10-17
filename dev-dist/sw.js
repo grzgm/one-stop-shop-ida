6c65cd198589a3fc09d1731a108de0659de6e796
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-10-17 15:21:45 +0200

    check if access token exists

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.31bnrha3fl"
+    "revision": "0.3glu5g2rqko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/sections/pages/Home.tsx
+++ src/sections/pages/Home.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [isAuth, setIsAuth] = useState(false)
+  useEffect(() => {
+    fetch(`http://localhost:3002/check-token`, {
+      method: 'GET',
+      credentials: 'include' // Include credentials (cookies) in the request
+  }).then(response => response.json())
+      .then(data => setIsAuth(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   return (
     <>
       <h1>Home</h1>
-      <Link to={`http://localhost:3002/microsoft/auth`}>Log in with Microsoft</Link>
+      {!isAuth && <Link to={`http://localhost:3002/microsoft/auth`}>Log in with Microsoft</Link>}
     </>
   );
 }
