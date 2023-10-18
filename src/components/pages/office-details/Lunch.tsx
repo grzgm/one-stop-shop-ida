889f860d3e2b9658285e69d501d5f5a0f456c530
Grzegorz <g.malisz@student.fontys.nl>
2023-10-18 09:22:26 +0200

    tab size fixed

--- src/components/pages/office-details/Lunch.tsx
+++ src/components/pages/office-details/Lunch.tsx
@@ -6,68 +6,68 @@ import { officeInformationData } from "../../../assets/OfficeInformationData";
 import { redirect } from "react-router-dom";
 
 function LunchLoader(officeName: string) {
-      const currentOfficeInformationData = officeInformationData[officeName]
-      if (currentOfficeInformationData && currentOfficeInformationData.canRegisterLunch == true) {
-            return null
-      }
-      throw redirect("/")
+	const currentOfficeInformationData = officeInformationData[officeName]
+	if (currentOfficeInformationData && currentOfficeInformationData.canRegisterLunch == true) {
+		return null
+	}
+	throw redirect("/")
 }
 
 function Lunch() {
-      const [weekRegistration, setWeekRegistration] = useState<boolean[]>([false, false, false, false, false]);
-      const weekDaysNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+	const [weekRegistration, setWeekRegistration] = useState<boolean[]>([false, false, false, false, false]);
+	const weekDaysNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-      const handleCheckboxChange = (index: number) => {
-            const updatedCheckedBoxes: boolean[] = [...weekRegistration];
-            updatedCheckedBoxes[index] = !updatedCheckedBoxes[index];
-            setWeekRegistration(updatedCheckedBoxes);
-      };
-      const saveLunchDays = () => {
-            console.log(weekRegistration)
-      };
-      const registerForToday = () => {
-            console.log("today register")
-      };
+	const handleCheckboxChange = (index: number) => {
+		const updatedCheckedBoxes: boolean[] = [...weekRegistration];
+		updatedCheckedBoxes[index] = !updatedCheckedBoxes[index];
+		setWeekRegistration(updatedCheckedBoxes);
+	};
+	const saveLunchDays = () => {
+		console.log(weekRegistration)
+	};
+	const registerForToday = () => {
+		console.log("today register")
+	};
 
-      return (
-            <div className="content">
-                  <div className="description">
-                        <HeadingLarge>Lunch</HeadingLarge>
-                        <BodyNormal>Don't forget to register!</BodyNormal>
-                  </div>
-                  <main className="lunch-main">
-                        <div className="lunch-main__recurring">
-                              <HeadingSmall>Register recurring</HeadingSmall>
-                              <BodySmall>Information will be sent</BodySmall>
-                              <BodySmall>before 12:00 on the mentioned day</BodySmall>
-                              <form className="lunch-main__form body--normal">
-                                    {weekRegistration.map((isChecked, index) => (
-                                          <div className="lunch-main__form__checkboxes" key={index}>
-                                                <input
-                                                      type="checkbox"
-                                                      checked={isChecked}
-                                                      onChange={() => handleCheckboxChange(index)}
-                                                      id={weekDaysNames[index]}
-                                                />
-                                                <label key={index} htmlFor={weekDaysNames[index]}>
-                                                      {weekDaysNames[index]}
-                                                </label>
-                                          </div>
-                                    ))}
-                              </form>
-                              <Button child="Save" onClick={() => saveLunchDays()} />
-                        </div>
-                        <div className="lunch-main__today">
-                              <HeadingSmall>Register for today</HeadingSmall>
-                              <BodySmall>Only for today</BodySmall>
-                              <BodySmall>before 12:00</BodySmall>
-                              <form>
-                                    <Button child="Register" onClick={() => registerForToday()} />
-                              </form>
-                        </div>
-                  </main>
-            </div>
-      );
+	return (
+		<div className="content">
+			<div className="description">
+				<HeadingLarge>Lunch</HeadingLarge>
+				<BodyNormal>Don't forget to register!</BodyNormal>
+			</div>
+			<main className="lunch-main">
+				<div className="lunch-main__recurring">
+					<HeadingSmall>Register recurring</HeadingSmall>
+					<BodySmall>Information will be sent</BodySmall>
+					<BodySmall>before 12:00 on the mentioned day</BodySmall>
+					<form className="lunch-main__form body--normal">
+						{weekRegistration.map((isChecked, index) => (
+							<div className="lunch-main__form__checkboxes" key={index}>
+								<input
+									type="checkbox"
+									checked={isChecked}
+									onChange={() => handleCheckboxChange(index)}
+									id={weekDaysNames[index]}
+								/>
+								<label key={index} htmlFor={weekDaysNames[index]}>
+									{weekDaysNames[index]}
+								</label>
+							</div>
+						))}
+					</form>
+					<Button child="Save" onClick={() => saveLunchDays()} />
+				</div>
+				<div className="lunch-main__today">
+					<HeadingSmall>Register for today</HeadingSmall>
+					<BodySmall>Only for today</BodySmall>
+					<BodySmall>before 12:00</BodySmall>
+					<form>
+						<Button child="Register" onClick={() => registerForToday()} />
+					</form>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default Lunch;
