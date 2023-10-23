332ccc92baa42a8f1654eb7c19e74ac2c58334ab
Grzegorz <g.malisz@student.fontys.nl>
2023-10-23 10:43:53 +0200

    company 101 page reshaped

--- src/components/pages/Company101.tsx
+++ src/components/pages/Company101.tsx
@@ -1,9 +1,23 @@
+import Button from "../Buttons";
+import { BodyNormal, HeadingLarge, HeadingSmall } from "../text-wrapers/TextWrapers";
+
 function Company101() {
 
-    return (
-          <h1>Company 101</h1>
-    );
-  }
-  
-  export default Company101;
-  
+	return (
+		<div className="content">
+			<div className="description">
+				<HeadingLarge>Company 101</HeadingLarge>
+				<BodyNormal>All you need to know when starting</BodyNormal>
+			</div>
+			<main>
+				<Button child="Initiation Course" onClick={() => window.open(`https://xploregroup.atlassian.net/wiki/spaces/IDANL/pages/8255046389/iDA+NL+employee+orientation+course`)} />
+				<section>
+					<HeadingSmall>Get you key card</HeadingSmall>
+					<BodyNormal>How to get your key card</BodyNormal>
+				</section>
+			</main>
+		</div>
+	);
+}
+
+export default Company101;
