3c26b2a6eb85fd13512ef0b38255cfa8ee76cfcb
Grzegorz <g.malisz@student.fontys.nl>
2023-10-23 12:31:55 +0200

    api endpoint unit tests set up

--- OneStopShopIdaBackend/Program.cs
+++ OneStopShopIdaBackend/Program.cs
@@ -52,3 +52,5 @@ app.UseSession();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
---
+++ UnitTests/MicrosoftGraphAPIAuthTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace UnitTests
+{
+    public class MicrosoftGraphAPIAuthTest
+    {
+        private readonly HttpClient _client;
+        public MicrosoftGraphAPIAuthTest() {
+            _client = new WebApplicationFactory<Program>().CreateClient();
+        }
+        [Fact]
+        public async void CheckTokenAPIEndpointTest()
+        {
+            var response = await _client.GetAsync("/microsoft/auth/check-token");
+            var data = await response.Content.ReadAsStringAsync();
+            //Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        //[Fact]
+        //public async void APIEndpointTest()
+        //{
+        //    var response = await _client.GetAsync("");
+        //    var data = await response.Content.ReadAsStringAsync();
+        //    //Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        //    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        //}
+
+    }
+}
