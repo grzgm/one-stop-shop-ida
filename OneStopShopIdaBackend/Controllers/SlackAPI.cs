b2ac27c26b743110207b6ded31c248c0c9908f00
Grzegorz <g.malisz@student.fontys.nl>
2023-10-25 11:30:44 +0200

    slack api sending messages

--- OneStopShopIdaBackend/Controllers/SlackAPI.cs
+++ OneStopShopIdaBackend/Controllers/SlackAPI.cs
@@ -12,13 +12,14 @@ namespace OneStopShopIdaBackend.Controllers
         private readonly CodeChallengeGeneratorService _codeChallengeGeneratorService;
 
         private const string SlackClientId = "12922529104.5894776759175";
-        private const string SlackClientSecret = "cf00d1d6730ca5b040d79309530ecef6";
+        private const string SlackClientSecret = "";
         //private const string Scopes = "channels%3Ahistory%2Cchannels%3Aread%2Cchat%3Awrite%2Cgroups%3Aread%2Cim%3Ahistory%2Cim%3Aread%2Cim%3Awrite%2Cmpim%3Ahistory%2Cmpim%3Aread%2Cusers.profile%3Aread%2Cusers.profile%3Awrite";
         private const string Scopes = "channels:history,channels:read,chat:write,groups:read,im:history,im:read,im:write,mpim:history,mpim:read,users.profile:read,users.profile:write";
 
         //private const string RedirectUri = "http://localhost:3002/slack/auth/callback";
-        private const string RedirectUri = "https://4d9f-2a02-a442-e502-1-8121-e1d-2252-9c68.ngrok-free.app/slack/auth/callback";
+        private const string RedirectUri = "https://221e-2a02-a442-e502-1-7489-89f7-8878-7b8f.ngrok-free.app/slack/auth/callback";
         private const string FrontendUri = "http://localhost:5173";
+        private const string slackAccessToken = "";
 
         public SlackAPIController(ILogger<SlackAPIController> logger, HttpClient httpClient, CodeChallengeGeneratorService codeChallengeGeneratorService)
         {
---
+++ OneStopShopIdaBackend/Controllers/SlackAPIResources.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneStopShopIdaBackend.Models;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Channels;
+
+namespace OneStopShopIdaBackend.Controllers
+{
+    public partial class SlackAPIController : ControllerBase
+    {
+        // OAuth Step 1: Redirect users to Slack's authorization URL
+        [HttpPost("send-message")]
+        public async Task<IActionResult> PostSendMessage([FromQuery] string message, [FromQuery] string channel)
+        {
+            try
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("https://slack.com/api/chat.postMessage"),
+                    Headers =
+                {
+                { "Authorization", $"Bearer {slackAccessToken}" },
+                },
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        { "channel", channel },
+                        { "text", message },
+                    }),
+                };
+
+                using (var response = await _httpClient.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    return StatusCode((int)response.StatusCode);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError($"Error calling external API: {ex.Message}");
+                return StatusCode(500, $"Internal Server Error \n {ex.Message}");
+            }
+        }
+
+        // OAuth Step 2: Handle the OAuth callback
+        [HttpPut("set-status")]
+        public async Task<IActionResult> PutSetStatus([FromQuery] string code, [FromQuery] string state)
+        {
+            try
+            {
+                var formData = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "code", code },
+                    { "client_id", SlackClientId },
+                    { "client_secret", SlackClientSecret }
+                });
+
+                var response = await _httpClient.PostAsync("https://slack.com/api/oauth.v2.access", formData);
+                response.EnsureSuccessStatusCode();
+
+                var responseData = await response.Content.ReadAsStringAsync();
+                dynamic responseObject = Newtonsoft.Json.JsonConvert.DeserializeObject(responseData);
+
+                // Access the access_token property
+                string slackAccessToken = responseObject.authed_user.access_token;
+
+                // Store accessToken and refreshToken in the session
+                HttpContext.Session.SetString("slackAccessToken", slackAccessToken);
+
+                return Redirect(FrontendUri + state);
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError($"Error calling external API: {ex.Message}");
+                return Redirect(FrontendUri + $"/slack-auth?serverResponse={JsonSerializer.Serialize(StatusCode(500, $"Internal Server Error \n {ex.Message}"))}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error calling external API: {ex.Message}");
+                return Redirect(FrontendUri + $"/slack-auth?serverResponse={JsonSerializer.Serialize(StatusCode(500, $"Internal Server Error \n {ex.Message}"))}");
+            }
+        }
+    }
+}
