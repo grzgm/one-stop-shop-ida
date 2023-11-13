c6a3e353524579c2cb38542c908b01b23b8ef9ef
Grzegorz <g.malisz@student.fontys.nl>
2023-11-13 08:28:30 +0100

    lunch recurring one file

--- OneStopShopIdaBackend/Models/LunchRecurringItem.cs
+++ OneStopShopIdaBackend/Models/LunchRecurringItem.cs
@@ -1,36 +1,43 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace OneStopShopIdaBackend.Models
+namespace OneStopShopIdaBackend.Models;
+public class LunchRecurringItem
 {
-    public class LunchRecurringItem
+    public LunchRecurringItem()
     {
-        public LunchRecurringItem()
-        {
-        }
-        public LunchRecurringItem(string microsoftId, LunchRecurringItemFrontend lunchRecurringItemFrontend)
-        {
-            this.MicrosoftId = microsoftId;
-            this.Monday = lunchRecurringItemFrontend.Monday;
-            this.Tuesday = lunchRecurringItemFrontend.Tuesday;
-            this.Wednesday = lunchRecurringItemFrontend.Wednesday;
-            this.Thursday = lunchRecurringItemFrontend.Thursday;
-            this.Friday = lunchRecurringItemFrontend.Friday;
-        }
-        [Key]
-        [ForeignKey(nameof(UserItem))]
-        [StringLength(255)]
-        [Required]
-        public string  MicrosoftId { get; set; }
-        [Required]
-        public bool Monday { get; set; }
-        [Required]
-        public bool Tuesday { get; set; }
-        [Required]
-        public bool Wednesday { get; set; }
-        [Required]
-        public bool Thursday { get; set; }
-        [Required]
-        public bool Friday { get; set; }
     }
+    public LunchRecurringItem(string microsoftId, LunchRecurringItemFrontend lunchRecurringItemFrontend)
+    {
+        this.MicrosoftId = microsoftId;
+        this.Monday = lunchRecurringItemFrontend.Monday;
+        this.Tuesday = lunchRecurringItemFrontend.Tuesday;
+        this.Wednesday = lunchRecurringItemFrontend.Wednesday;
+        this.Thursday = lunchRecurringItemFrontend.Thursday;
+        this.Friday = lunchRecurringItemFrontend.Friday;
+    }
+    [Key]
+    [ForeignKey(nameof(UserItem))]
+    [StringLength(255)]
+    [Required]
+    public string MicrosoftId { get; set; }
+    [Required]
+    public bool Monday { get; set; }
+    [Required]
+    public bool Tuesday { get; set; }
+    [Required]
+    public bool Wednesday { get; set; }
+    [Required]
+    public bool Thursday { get; set; }
+    [Required]
+    public bool Friday { get; set; }
+}
+
+public class LunchRecurringItemFrontend
+{
+    public bool Monday { get; set; }
+    public bool Tuesday { get; set; }
+    public bool Wednesday { get; set; }
+    public bool Thursday { get; set; }
+    public bool Friday { get; set; }
 }
