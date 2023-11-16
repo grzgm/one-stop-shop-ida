6d8cfb7a9b9c3aa5da28cc7f36199946582da379
Grzegorz <g.malisz@student.fontys.nl>
2023-11-16 18:16:12 +0100

    NUnit and Playwrite setup

---
+++ TestsNUnit/FakeServices/DatabaseServiceFake.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OneStopShopIdaBackend.Models;
+using OneStopShopIdaBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsNUnit.FakeServices;
+internal class DatabaseServiceFake : IDatabaseService
+{
+    public DatabaseServiceFake()
+    {
+    }
+
+    public DbSet<LunchRecurringItem> LunchRecurring { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DbSet<LunchRecurringRegistrationItem> LunchRecurringRegistration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DbSet<LunchTodayItem> LunchToday { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DbSet<PushSubscription> PushSubscription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DbSet<UserItem> Users { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void CheckOrGenerateVapidDetails(string vapidSubject, string vapidPublicKey, string vapidPrivateKey)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteUserItem(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DateTime> GetLunchRecurringRegistrationLastRegistered(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> GetLunchTodayIsRegistered(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LunchRecurringItem> GetRegisteredDays(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserItem> GetUserItem(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserItem>> GetUserItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetVapidPublicKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsSubscribe(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PostLunchRecurringItem(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PostLunchRecurringRegistrationItem(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PostLunchTodayItem(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PostUserItem(UserItem userItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PutLunchRecurringItem(LunchRecurringItem lunchRecurringItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PutLunchRecurringRegistrationItem(LunchRecurringRegistrationItem lunchRecurringRegistrationItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PutLunchTodayRegister(LunchTodayItem lunchTodayItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PutUserItem(UserItem userItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendNotificationsToUsers(Notification notification, List<UserItem> userItems)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PushSubscription> Subscribe(PushSubscription subscription, string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Unsubscribe(PushSubscription subscription)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAllLunchRecurringRegistrationItems(DateTime lastRegistered)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAllLunchTodayItems(bool isRegistered)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UserItemExists(string microsoftId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TestsNUnit/FakeServices/MicrosoftGraphAPIServiceFake.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using OneStopShopIdaBackend.Models;
+using OneStopShopIdaBackend.Services;
+
+namespace TestsNUnit.FakeServices;
+public class MicrosoftGraphApiServiceFake : IMicrosoftGraphApiService
+{
+    public MicrosoftGraphApiServiceFake()
+    {
+    }
+
+    public async Task<(string, string)> CallAuthCallback(string code, string state)
+    {
+        return ("test", "test");
+    }
+
+    public Task<(string, string)> CallAuthRefresh(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<HttpResponseMessage> CreateEvent(string accessToken, string address, string title, string startDate, string endDate, string description)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateMicrosoftGraphAPIAuthUrl(string route)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserItem> GetMe(string accessToken)
+    {
+        return new UserItem() { MicrosoftId = "test", FirstName = "test", Surname = "test", Email = "test" };
+    }
+
+    public Task<HttpResponseMessage> RegisterLunchRecurring(string accessToken, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<HttpResponseMessage> RegisterLunchToday(string accessToken, string microsoftId, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<HttpResponseMessage> SendEmail(string accessToken, string message, string address)
+    {
+        throw new NotImplementedException();
+    }
+}
