c3c7a04ee180b9aed3b3711f9df1ef8e1b718640
Grzegorz <g.malisz@student.fontys.nl>
2023-11-20 13:13:33 +0100

    integration tests ms auth process

--- TestsNUnit/MicrosoftGraphApiControllerIntegrationTests.cs
+++ TestsNUnit/MicrosoftGraphApiControllerIntegrationTests.cs
@@ -1,171 +1,55 @@
-﻿using Microsoft.Extensions.Logging;
-using OneStopShopIdaBackend.Controllers;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using OneStopShopIdaBackend.Models;
-using TestsNUnit.FakeServices;
-using System.Text;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Org.BouncyCastle.Utilities.Collections;
 
 namespace TestsNUnit;
 
+[Parallelizable(ParallelScope.Self)]
 [TestFixture]
-internal class MicrosoftGraphApiControllerIntegrationTests
+internal class MicrosoftGraphApiControllerIntegrationTests : PageTest
 {
-    private DatabaseServiceFake _databaseServiceFake;
-    private MicrosoftGraphApiController _microsoftGraphApiController;
-
     [SetUp]
     public void Setup()
     {
-        // Create Fake Database
-        var options = new DbContextOptionsBuilder<DatabaseServiceFake>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
-        _databaseServiceFake = new DatabaseServiceFake(options);
-        // Clear database
-        _databaseServiceFake.Database.EnsureDeleted();
-        _databaseServiceFake.Database.EnsureCreated();
 
-        // Create Fake Microsoft Graph Api Service
-        MicrosoftGraphApiServiceFake microsoftGraphApiServiceFake = new();
-
-        // Initialise Microsoft Graph Api Controller
-        _microsoftGraphApiController = new MicrosoftGraphApiController(
-            new Logger<MicrosoftGraphApiController>(new LoggerFactory()), microsoftGraphApiServiceFake,
-            _databaseServiceFake);
     }
 
     [Test]
-    // Check whether Controller saves user information to session correctly
-    public async Task GetAuthCallbackUserInDatabaseTest()
+    public async Task MicrosoftAuthTest()
     {
-        // Arrange
-        // Add Test User Item to Fake Database
-        await _databaseServiceFake.PostUserItem(ModelsObjectsFake.testUserItem);
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        IActionResult response;
+        await Page.GotoAsync("http://localhost:3002/microsoft/auth?route=/office-details/lunch");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        // Expect a title "to contain" a substring.
+        //await Expect(Page).ToHaveTitleAsync(new Regex("Sign in to your account"));
 
-        // Act
-        response = await _microsoftGraphApiController.GetAuthCallback("testCode", "testState");
+        // create a locator
+        await Page.WaitForTimeoutAsync(1000);
+        var emailInput = await Page.QuerySelectorAsync("input[type=email]");
+        await emailInput.FillAsync("");
 
-        // Assert
-        Assert.AreEqual(ModelsObjectsFake.testUserItem.MicrosoftId, _microsoftGraphApiController.HttpContext.Session.GetString("microsoftId"));
-    }
+        var button = await Page.QuerySelectorAsync("input[type=submit]");
+        await button.ClickAsync();
 
-    [Test]
-    // Check whether Controller saves new user information to database correctly
-    public async Task GetAuthCallbackUserNotInDatabaseTest()
-    {
-        // Arrange
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        IActionResult response;
+        // Password page
+        await Page.WaitForTimeoutAsync(2000);
+        var passwordInput = await Page.QuerySelectorAsync("input[type=password]");
+        await passwordInput.FillAsync("");
 
-        // Act
-        response = await _microsoftGraphApiController.GetAuthCallback("testCode", "testState");
-        UserItem userItem = await _databaseServiceFake.GetUserItem(ModelsObjectsFake.testUserItem.MicrosoftId);
-        LunchRecurringItem lunchRecurringItem =
-            await _databaseServiceFake.GetRegisteredDays(ModelsObjectsFake.testUserItem.MicrosoftId);
+        button = await Page.QuerySelectorAsync("input[type=submit]");
+        await button.ClickAsync();
 
-        // Assert
-        Assert.AreEqual(ModelsObjectsFake.testUserItem.MicrosoftId, userItem.MicrosoftId);
-        Assert.AreEqual(ModelsObjectsFake.testLunchRecurringItem.MicrosoftId, lunchRecurringItem.MicrosoftId);
-    }
+        // Microsoft Two step Auth
+        //string divTextContent = await Page.TextContentAsync("#idRichContext_DisplaySign");
 
-    [Test]
-    // Check whether Controller saves user information to session correctly
-    public async Task GetAuthCallbackUserAuthorisedAlready()
-    {
-        // Arrange
-        // Add Test User Item to Fake Database
-        await _databaseServiceFake.PostUserItem(ModelsObjectsFake.testUserItem);
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        httpContext.Session.SetString("accessToken", "oldAccessToken");
-        httpContext.Session.SetString("refreshToken", "oldRefreshToken");
-        httpContext.Session.SetString("microsoftId", "oldMicrosoftId");
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        IActionResult response;
+        await Page.WaitForTimeoutAsync(1000);
 
-        // Act
-        response = await _microsoftGraphApiController.GetAuthCallback("testCode", "testState");
+        button = await Page.QuerySelectorAsync("input[type=submit][value=Tak]");
+        await button.ClickAsync();
 
-        // Assert
-        Assert.AreNotEqual("oldAccessToken", _microsoftGraphApiController.HttpContext.Session.GetString("accessToken"));
-        Assert.AreNotEqual("oldRefreshToken", _microsoftGraphApiController.HttpContext.Session.GetString("refreshToken"));
-        Assert.AreNotEqual("oldMicrosoftId", _microsoftGraphApiController.HttpContext.Session.GetString("microsoftId"));
-    }
-
-
-    [Test]
-    // Check whether Controller saves new user information to database correctly
-    public async Task GetAuthRefreshTest()
-    {
-        // Arrange
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        httpContext.Session.SetString("accessToken", "oldAccessToken");
-        httpContext.Session.SetString("refreshToken", "oldRefreshToken");
-        httpContext.Session.SetString("microsoftId", "oldMicrosoftId");
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        IActionResult response;
-
-        // Act
-        response = await _microsoftGraphApiController.GetAuthRefresh();
-
-        // Assert
-        Assert.AreNotEqual("oldAccessToken", _microsoftGraphApiController.HttpContext.Session.GetString("accessToken"));
-        Assert.AreNotEqual("oldRefreshToken", _microsoftGraphApiController.HttpContext.Session.GetString("refreshToken"));
-    }
-
-    [Test]
-    public async Task GetCheckTokenTokenInSessionTest()
-    {
-        // Arrange
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        httpContext.Session.SetString("accessToken", "oldAccessToken");
-        httpContext.Session.SetString("refreshToken", "oldRefreshToken");
-        httpContext.Session.SetString("microsoftId", "oldMicrosoftId");
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        ActionResult<bool> response;
-
-        // Act
-        response = await _microsoftGraphApiController.GetCheckToken();
-        var okObjectResult = response.Result as OkObjectResult;
-        var isToken = (bool)okObjectResult.Value;
-
-        // Assert
-        Assert.IsNotNull(okObjectResult);
-        Assert.AreEqual(true, isToken);
-    }
-
-    [Test]
-    public async Task GetCheckTokenTokenNotInSessionTest()
-    {
-        // Arrange
-        // Fake Session
-        var httpContext = HttpContextFake.GetHttpContextFake();
-        _microsoftGraphApiController.ControllerContext = new ControllerContext() { HttpContext = httpContext };
-        // Controller Response
-        ActionResult<bool> response;
-
-        // Act
-        response = await _microsoftGraphApiController.GetCheckToken();
-        var okObjectResult = response.Result as OkObjectResult;
-        var isToken = (bool)okObjectResult.Value;
-
-        // Assert
-        Assert.IsNotNull(okObjectResult);
-        Assert.AreEqual(false, isToken);
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        await Page.WaitForTimeoutAsync(1000);
+        await Expect(Page).ToHaveURLAsync(new Regex("/office-details/lunch"));
+        //await Expect(Page).ToHaveTitleAsync(new Regex("Sign in to your account"));
     }
 }
