27c79232b3e39505c8acfdd421c13448c9a1518f
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-11-21 14:11:28 +0100

    name changing, code clean up

--- src/components/bars/Navbar.tsx
+++ src/components/bars/Navbar.tsx
@@ -5,32 +5,32 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { Dispatch, SetStateAction } from "react";
 
 interface NavbarProps {
-  onPressOpenSidebar: Dispatch<SetStateAction<boolean>>;
-  navbarOptionsRef: React.RefObject<HTMLDivElement>;
+	onPressOpenSidebar: Dispatch<SetStateAction<boolean>>;
+	navbarOptionsRef: React.RefObject<HTMLDivElement>;
 }
 
 function Navbar({ onPressOpenSidebar, navbarOptionsRef }: NavbarProps) {
-  return (
-    <nav id="navbar">
-      <Link to="/"><img id="navbar__logo" src={idaLogo} alt="iDA" /></Link>
-      <div id="navbar__options" ref={navbarOptionsRef}>
-        <div id="navbar__options__left" className="heading--small">
-          <Link className="navbar__option heading--small" to="/">Home</Link>
-          <Link className="navbar__option heading--small" to="/employee-portal">EmployeePortal</Link>
-          <Link className="navbar__option heading--small" to="/office-details">Office Details</Link>
-          <Link className="navbar__option heading--small" to="/company101">Company 101</Link>
-          <Link className="navbar__option heading--small" to="/personal-skills">Personal Skills</Link>
-          <Link className="navbar__option heading--small" to="https://accounts.rydoo.com/login?signin=4bfbb925942188e56808788796a0fe72" target="_blank" rel="noopener noreferrer">Expenses</Link>
-        </div>
-        <div className="navbar__burger-menu" onClick={() => onPressOpenSidebar(true)}><MenuIcon fontSize="inherit" /></div>
-        <div id="navbar__separator" />
-        <div id="navbar__options__right">
-          <Link className="navbar__option heading--small" to="/offices">Change Offices</Link>
-          <Link className="navbar__option heading--small" to="/settings">Settings</Link>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav id="navbar">
+			<Link to="/"><img id="navbar__logo" src={idaLogo} alt="iDA" /></Link>
+			<div id="navbar__options" ref={navbarOptionsRef}>
+				<div id="navbar__options__left" className="heading--small">
+					<Link className="navbar__option heading--small" to="/">Home</Link>
+					<Link className="navbar__option heading--small" to="/employee-portal">EmployeePortal</Link>
+					<Link className="navbar__option heading--small" to="/office-details">Office Details</Link>
+					<Link className="navbar__option heading--small" to="/company101">Company 101</Link>
+					<Link className="navbar__option heading--small" to="/personal-skills">Personal Skills</Link>
+					<Link className="navbar__option heading--small" to="https://accounts.rydoo.com/login?signin=4bfbb925942188e56808788796a0fe72" target="_blank" rel="noopener noreferrer">Expenses</Link>
+				</div>
+				<div className="navbar__burger-menu" onClick={() => onPressOpenSidebar(true)}><MenuIcon fontSize="inherit" /></div>
+				<div id="navbar__separator" />
+				<div id="navbar__options__right">
+					<Link className="navbar__option heading--small" to="/offices">Offices</Link>
+					<Link className="navbar__option heading--small" to="/settings">Settings</Link>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
--- src/components/bars/Sidebar.tsx
+++ src/components/bars/Sidebar.tsx
@@ -5,80 +5,80 @@ import CloseIcon from "@mui/icons-material/Close";
 import { Dispatch, SetStateAction } from "react";
 
 interface SidebarProps {
-  onPressCloseSidebar: Dispatch<SetStateAction<boolean>>;
+	onPressCloseSidebar: Dispatch<SetStateAction<boolean>>;
 }
 
 function Sidebar({ onPressCloseSidebar }: SidebarProps) {
-  return (
-    <>
-      <nav id="sidebar">
-        {/* <div className="sidebar__burger-menu heading--large">
-        <MenuIcon fontSize="inherit" />
-      </div> */}
-        <div
-          className="sidebar__burger-menu heading--large"
-          onClick={() => onPressCloseSidebar(false)}
-        >
-          <CloseIcon fontSize="inherit" />
-        </div>
-        <div id="sidebar__options">
-          <div id="sidebar__options__top" className="heading--small">
-            <Link className="sidebar__option heading--small" to="/" onClick={() => onPressCloseSidebar(false)}>
-              Home
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="/employee-portal" onClick={() => onPressCloseSidebar(false)}>
-              EmployeePortal
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="/office-details" onClick={() => onPressCloseSidebar(false)}>
-              Office Details
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="/company101" onClick={() => onPressCloseSidebar(false)}>
-              Company 101
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="/personal-skills" onClick={() => onPressCloseSidebar(false)}>
-              Personal Skills
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="https://accounts.rydoo.com/login?signin=4bfbb925942188e56808788796a0fe72" target="_blank" rel="noopener noreferrer" onClick={() => onPressCloseSidebar(false)}>
-              Expenses
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-          </div>
-          <div id="sidebar__options__bottom">
-            <Link className="sidebar__option heading--small" to="/offices" onClick={() => onPressCloseSidebar(false)}>
-              Change Offices
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-            <Link className="sidebar__option heading--small" to="/settings" onClick={() => onPressCloseSidebar(false)}>
-              Settings
-              <div className="sidebar__arrow heading--large">
-                <KeyboardArrowRightIcon fontSize="inherit" />
-              </div>
-            </Link>
-          </div>
-        </div>
-      </nav>
-      <div id="cover" onClick={() => onPressCloseSidebar(false)} />
-    </>
-  );
+	return (
+		<>
+			<nav id="sidebar">
+				{/* <div className="sidebar__burger-menu heading--large">
+						<MenuIcon fontSize="inherit" />
+					</div> */}
+				<div
+					className="sidebar__burger-menu heading--large"
+					onClick={() => onPressCloseSidebar(false)}
+				>
+					<CloseIcon fontSize="inherit" />
+				</div>
+				<div id="sidebar__options">
+					<div id="sidebar__options__top" className="heading--small">
+						<Link className="sidebar__option heading--small" to="/" onClick={() => onPressCloseSidebar(false)}>
+							Home
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="/employee-portal" onClick={() => onPressCloseSidebar(false)}>
+							EmployeePortal
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="/office-details" onClick={() => onPressCloseSidebar(false)}>
+							Office Details
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="/company101" onClick={() => onPressCloseSidebar(false)}>
+							Company 101
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="/personal-skills" onClick={() => onPressCloseSidebar(false)}>
+							Personal Skills
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="https://accounts.rydoo.com/login?signin=4bfbb925942188e56808788796a0fe72" target="_blank" rel="noopener noreferrer" onClick={() => onPressCloseSidebar(false)}>
+							Expenses
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+					</div>
+					<div id="sidebar__options__bottom">
+						<Link className="sidebar__option heading--small" to="/offices" onClick={() => onPressCloseSidebar(false)}>
+							Offices
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+						<Link className="sidebar__option heading--small" to="/settings" onClick={() => onPressCloseSidebar(false)}>
+							Settings
+							<div className="sidebar__arrow heading--large">
+								<KeyboardArrowRightIcon fontSize="inherit" />
+							</div>
+						</Link>
+					</div>
+				</div>
+			</nav>
+			<div id="cover" onClick={() => onPressCloseSidebar(false)} />
+		</>
+	);
 }
 
 export default Sidebar;
