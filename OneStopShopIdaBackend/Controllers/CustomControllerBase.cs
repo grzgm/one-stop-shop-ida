ad553326d75018894c6b92cc0c6e1c8ee38b16ae
Grzegorz <g.malisz@student.fontys.nl>
2023-11-28 11:35:07 +0100

    backend token refresh

---
+++ OneStopShopIdaBackend/Controllers/CustomControllerBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneStopShopIdaBackend.Services;
+
+namespace OneStopShopIdaBackend.Controllers;
+public class CustomControllerBase : ControllerBase
+{
+    protected readonly IMicrosoftGraphApiService _microsoftGraphApiService;
+    public CustomControllerBase(IMicrosoftGraphApiService microsoftGraphApiService)
+    {
+        _microsoftGraphApiService = microsoftGraphApiService;
+    }
+
+    public async Task<T> ExecuteWithRetry<T>(Func<Task<T>> action)
+    {
+        try
+        {
+            return await action();
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                var t = HttpContext.Session.GetString("refreshToken");
+                // Access the access_token property
+                (string accessToken, string refreshToken) = await _microsoftGraphApiService.CallAuthRefresh(HttpContext.Session.GetString("refreshToken"));
+
+                // Store accessToken and refreshToken in the session
+                HttpContext.Session.SetString("accessToken", accessToken);
+                HttpContext.Session.SetString("refreshToken", refreshToken);
+                return await action();
+            }
+            throw;
+        }
+    }
+}
--- OneStopShopIdaBackend/Controllers/LunchTodayItemsController.cs
+++ OneStopShopIdaBackend/Controllers/LunchTodayItemsController.cs
@@ -7,18 +7,16 @@ namespace OneStopShopIdaBackend.Controllers;
 
 [Route("lunch/today")]
 [ApiController]
-public class LunchTodayItemsController : ControllerBase
+public class LunchTodayItemsController : CustomControllerBase
 {
     private readonly ILogger<LunchTodayItemsController> _logger;
-    private readonly IMicrosoftGraphApiService _microsoftGraphApiService;
     private readonly IDatabaseService _databaseService;
 
     public LunchTodayItemsController(ILogger<LunchTodayItemsController> logger,
-        IMicrosoftGraphApiService microsoftGraphApiService, IDatabaseService databaseService)
+        IMicrosoftGraphApiService microsoftGraphApiService, IDatabaseService databaseService) : base(microsoftGraphApiService)
     {
         _logger = logger;
         _databaseService = databaseService;
-        _microsoftGraphApiService = microsoftGraphApiService;
     }
     private static string RegisterTodayMessage(string officeName, string name) =>
         "Hi,\n" +
@@ -30,7 +28,7 @@ public class LunchTodayItemsController : ControllerBase
     public async Task<ActionResult<bool>> GetLunchTodayIsRegistered()
     {
         string accessToken = HttpContext.Session.GetString("accessToken");
-        string microsoftId = (await _microsoftGraphApiService.GetMe(accessToken)).MicrosoftId;
+        string microsoftId = (await ExecuteWithRetry(async () => await _microsoftGraphApiService.GetMe(accessToken))).MicrosoftId;
 
         return await _databaseService.GetLunchTodayIsRegistered(microsoftId);
     }
@@ -39,7 +37,8 @@ public class LunchTodayItemsController : ControllerBase
     public async Task<IActionResult> PutLunchTodayRegister([FromQuery] string officeName)
     {
         string accessToken = HttpContext.Session.GetString("accessToken");
-        string microsoftId = (await _microsoftGraphApiService.GetMe(accessToken)).MicrosoftId;
+
+        string microsoftId = (await ExecuteWithRetry(async () => await _microsoftGraphApiService.GetMe(accessToken))).MicrosoftId;
 
         var user = await _microsoftGraphApiService.GetMe(accessToken);
         HttpResponseMessage response = await
