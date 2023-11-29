ea31c681170cc777ba156e532c97f9179a532f8f
Grzegorz <g.malisz@student.fontys.nl>
2023-11-29 09:47:08 +0100

    unused sidebar context removed

--- src/contexts/SidebarContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext, useState } from 'react';
-
-const [showSidebar, setShowSidebar] = useState(false)
-
-export const ShowSidebarContext = createContext({showSidebar: showSidebar, switchShowSidebar: setShowSidebar})
