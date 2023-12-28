9dcd6e774994f6ca80d69ac0c277f645d52f0b5b
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-12-28 12:24:25 +0100

    timeslots text info

--- src/components/pages/office-details/ReserveDesk.tsx
+++ src/components/pages/office-details/ReserveDesk.tsx
@@ -1,35 +1,36 @@
 import OfficeSpace from "../../OfficeSpace";
-import { BodyNormal, HeadingLarge } from "../../text-wrapers/TextWrapers";
+import { BodyNormal, BodySmall, HeadingLarge } from "../../text-wrapers/TextWrapers";
 import "../../../css/components/pages/office-details/reserve-desk.css"
 import { officeInformationData } from "../../../assets/OfficeInformationData";
 import { redirect } from "react-router-dom";
 import { IsAuth } from "../../../api/MicrosoftGraphAPI";
 
 async function ReserveDeskLoader(officeName: string) {
-  const currentOfficeInformationData = officeInformationData[officeName]
-  if (currentOfficeInformationData && currentOfficeInformationData.canReserveDesk == true) {
+	const currentOfficeInformationData = officeInformationData[officeName]
+	if (currentOfficeInformationData && currentOfficeInformationData.canReserveDesk == true) {
 		if ((await IsAuth()).payload) {
 			return null
 		}
 		else {
 			return redirect(`/microsoft-auth?previousLocation=${encodeURI("/office-details/reserve-desk")}`)
 		}
-  }
-  throw redirect("/")
+	}
+	throw redirect("/")
 }
 
 function ReserveDesk() {
-  return (
-    <div className="content">
-      <div className="description">
-        <HeadingLarge>Reserve a Desk</HeadingLarge>
-        <BodyNormal>Choose time slot</BodyNormal>
-      </div>
-      <main className="reserve-desk-main">
-        <OfficeSpace />
-      </main>
-    </div>
-  );
+	return (
+		<div className="content">
+			<div className="description">
+				<HeadingLarge>Reserve a Desk</HeadingLarge>
+				<BodyNormal>Choose time slot</BodyNormal>
+				<BodySmall>Remember you cannot have two identical time slots</BodySmall>
+			</div>
+			<main className="reserve-desk-main">
+				<OfficeSpace />
+			</main>
+		</div>
+	);
 }
 
 export default ReserveDesk;
