9e085b80a1fde4f65dd53282b8d922f974375413
Grzegorz Malisz <g.malisz@student.fontys.nl>
2024-01-09 23:17:23 +0100

    body typo fix

--- src/api/Request.ts
+++ src/api/Request.ts
@@ -4,20 +4,21 @@ import { redirect } from "react-router-dom";
 
 
 async function ExecuteApiCall<T>(endpointPath: string, method: string, body?: string) {
-    body = body ?? '';
+    const options: RequestInit =
+    {
+        method: method,
+        credentials: "include", // Include credentials (cookies) in the request
+        headers: {
+            'Authorization': `Bearer ${new Cookies().get("jwt")}`,
+            'Content-Type': 'application/json',
+        },
+        body: body,
+    }
 
-    const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URI}${endpointPath}`,
-        {
-            method: method,
-            credentials: "include", // Include credentials (cookies) in the request
-            headers: {
-                'Authorization': `Bearer ${new Cookies().get("jwt")}`,
-                'Content-Type': 'application/json',
-                'Body': body,
-            }
-        }
-    );
+    if (body)
+        options.body = body;
+
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URI}${endpointPath}`, options);
 
     const inspectedRes = await InspectResponseAsync<T>(res);
 
