b0c8700ea97b4040df0c9bd224ddeeafdc02e352
Grzegorz Malisz <g.malisz@student.fontys.nl>
2024-01-11 10:05:25 +0100

    ms graph api unused functions commented

--- src/api/MicrosoftGraphAPI.tsx
+++ src/api/MicrosoftGraphAPI.tsx
@@ -19,22 +19,23 @@ async function AuthUrl(): Promise<IActionResult<string>> {
 	}
 }
 
-async function SendEmail(message: string, address: string): Promise<IActionResult<undefined>> {
-	try {
-		return await ExecuteApiCall<undefined>(`/microsoft/resources/send-email?message=${encodeURI(message)}&address=${encodeURI(address)}`, "POST");
-	} catch (error) {
-		console.error("Error:", error);
-		return { success: false, statusText: "Request could not be send." };
-	}
-}
+// async function SendEmail(message: string, address: string): Promise<IActionResult<undefined>> {
+// 	try {
+// 		return await ExecuteApiCall<undefined>(`/microsoft/resources/send-email?message=${encodeURI(message)}&address=${encodeURI(address)}`, "POST");
+// 	} catch (error) {
+// 		console.error("Error:", error);
+// 		return { success: false, statusText: "Request could not be send." };
+// 	}
+// }
 
-async function CreateEvent(address: string, title: string, startDate: string, endDate: string, description?: string): Promise<IActionResult<undefined>> {
-	try {
-		return await ExecuteApiCall<undefined>(`/microsoft/resources/create-event?address=${encodeURI(address)}&title=${title}&startDate=${startDate}&endDate=${endDate}&description=${description}`, "GET");
-	} catch (error) {
-		console.error("Error:", error);
-		return { success: false, statusText: "Request could not be send." };
-	}
-}
+// async function CreateEvent(address: string, title: string, startDate: string, endDate: string, description?: string): Promise<IActionResult<undefined>> {
+// 	try {
+// 		return await ExecuteApiCall<undefined>(`/microsoft/resources/create-event?address=${encodeURI(address)}&title=${title}&startDate=${startDate}&endDate=${endDate}&description=${description}`, "GET");
+// 	} catch (error) {
+// 		console.error("Error:", error);
+// 		return { success: false, statusText: "Request could not be send." };
+// 	}
+// }
 
-export { IsAuth, AuthUrl, SendEmail, CreateEvent };
+// export { IsAuth, AuthUrl, SendEmail, CreateEvent };
+export { IsAuth, AuthUrl };
