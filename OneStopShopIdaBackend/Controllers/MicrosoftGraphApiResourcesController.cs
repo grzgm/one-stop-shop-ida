841807c50e3064479d25741a788ef69b8a8aa359
Grzegorz Malisz <g.malisz@student.fontys.nl>
2024-01-11 10:06:40 +0100

    ms graph api unused functions commented

--- OneStopShopIdaBackend/Controllers/MicrosoftGraphApiResourcesController.cs
+++ OneStopShopIdaBackend/Controllers/MicrosoftGraphApiResourcesController.cs
@@ -5,30 +5,30 @@ namespace OneStopShopIdaBackend.Controllers;
 
 public partial class MicrosoftGraphApiController
 {
-    [HttpPost("resources/send-email")]
-    public async Task<IActionResult> PostSendEmail([FromQuery] string address, [FromQuery] string subject,
-        [FromQuery] string message)
-    {
-        string accessToken = _memoryCache.Get<string>($"{User.FindFirst("UserId").Value}AccessToken");
-
-        HttpResponseMessage response = await ExecuteWithRetryMicrosoftGraphApi(
-            async (accessToken) => await
-                _microsoftGraphApiService.SendEmail(accessToken, address, subject, message), accessToken);
-        return StatusCode((int)response.StatusCode);
-    }
-
-    [HttpPost("resources/create-event")]
-    public async Task<IActionResult> PostCreateEvent([FromQuery] string address, [FromQuery] string title,
-        [FromQuery] string startDate, [FromQuery] string endDate, [FromQuery] string description)
-    {
-        string accessToken = _memoryCache.Get<string>($"{User.FindFirst("UserId").Value}AccessToken");
-
-        HttpResponseMessage response = await ExecuteWithRetryMicrosoftGraphApi(async (accessToken) =>
-            await _microsoftGraphApiService.CreateEvent(accessToken, address, title,
-                startDate, endDate, description), accessToken);
-
-        return StatusCode((int)response.StatusCode);
-    }
+    // [HttpPost("resources/send-email")]
+    // public async Task<IActionResult> PostSendEmail([FromQuery] string address, [FromQuery] string subject,
+    //     [FromQuery] string message)
+    // {
+    //     string accessToken = _memoryCache.Get<string>($"{User.FindFirst("UserId").Value}AccessToken");
+    //
+    //     HttpResponseMessage response = await ExecuteWithRetryMicrosoftGraphApi(
+    //         async (accessToken) => await
+    //             _microsoftGraphApiService.SendEmail(accessToken, address, subject, message), accessToken);
+    //     return StatusCode((int)response.StatusCode);
+    // }
+    //
+    // [HttpPost("resources/create-event")]
+    // public async Task<IActionResult> PostCreateEvent([FromQuery] string address, [FromQuery] string title,
+    //     [FromQuery] string startDate, [FromQuery] string endDate, [FromQuery] string description)
+    // {
+    //     string accessToken = _memoryCache.Get<string>($"{User.FindFirst("UserId").Value}AccessToken");
+    //
+    //     HttpResponseMessage response = await ExecuteWithRetryMicrosoftGraphApi(async (accessToken) =>
+    //         await _microsoftGraphApiService.CreateEvent(accessToken, address, title,
+    //             startDate, endDate, description), accessToken);
+    //
+    //     return StatusCode((int)response.StatusCode);
+    // }
 
     //[HttpGet("resources/me")]
     //public async Task<UserItem> GetMe(string accessToken)
