d3e636ed16aebbbd1597492ecce4d620331e892a
Grzegorz Malisz <g.malisz@student.fontys.nl>
2024-01-12 16:01:12 +0100

    dynamic info for offices

--- src/components/pages/office-details/OfficeInformation.tsx
+++ src/components/pages/office-details/OfficeInformation.tsx
@@ -7,39 +7,39 @@ import { officeInformationUtrechtDefaultData } from "../../../assets/OfficeInfor
 
 function OfficeInformation() {
 	const officeName = useContext(CurrentOfficeContext).currentOffice ?? 'Utrecht';
-	const { officeFeatures } = useContext(OfficeFeaturesContext) ?? {officeInformationUtrechtDefaultData};
+	const { officeFeatures } = useContext(OfficeFeaturesContext) ?? { officeInformationUtrechtDefaultData };
 	const currentOfficeInformationData = officeFeatures[officeName.toLocaleLowerCase()]
 
 	return (
 		<div className="content">
 			<div className="description">
-				<HeadingLarge>{`${officeName} Office Information`}</HeadingLarge>
+				<HeadingLarge>{`${capitalizeFirstLetter(officeName)} Office Information`}</HeadingLarge>
 				<BodyNormal>All information you need!</BodyNormal>
 			</ div>
 			<main id="office-information-main">
-				<section>
-					<HeadingSmall>Address</HeadingSmall>
-					<BodyNormal>{currentOfficeInformationData.officeInformation.address}</BodyNormal>
-				</section>
-				<section>
-					<HeadingSmall>Opening Hours</HeadingSmall>
-					<BodyNormal>{currentOfficeInformationData.officeInformation.openingHours}</BodyNormal>
-				</section>
-				<section>
-					<HeadingSmall>Access Information</HeadingSmall>
-					<BodyNormal>{currentOfficeInformationData.officeInformation.accessInformation}</BodyNormal>
-				</section>
-				<section>
-					<HeadingSmall>Parking Information</HeadingSmall>
-					<BodyNormal>{currentOfficeInformationData.officeInformation.parkingInformation}</BodyNormal>
-				</section>
-				{currentOfficeInformationData.canRegisterLunch && <section>
-					<HeadingSmall>Lunch Information</HeadingSmall>
-					<BodyNormal>{currentOfficeInformationData.officeInformation.lunchInformation}</BodyNormal>
-				</section>}
+
+				{(Object.entries(currentOfficeInformationData.officeInformation)).map(([information, value]) => {
+					if (information == "officeName" || information == "officeCoordinates" || !value)
+						return
+					return (
+						<section>
+							<HeadingSmall>{capitalizeFirstLetter(addSpaceBeforeCapitalLetters(information.toString()))}</HeadingSmall>
+							<BodyNormal>{value.toString()}</BodyNormal>
+						</section>)
+				})}
+
 			</main>
 		</ div>
 	);
+}
+function capitalizeFirstLetter(str: string) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function addSpaceBeforeCapitalLetters(str: string) {
+	// Use a regular expression to match capital letters
+	// and insert a space before them using replace
+	return str.replace(/([A-Z])/g, ' $1');
 }
 
 export default OfficeInformation;
