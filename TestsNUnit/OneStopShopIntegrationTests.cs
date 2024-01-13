304e12d95b294bbfa74be4e21312fb6f2ca83536
Grzegorz <g.malisz@student.fontys.nl>
2024-01-13 10:37:55 +0100

    name update

---
+++ TestsNUnit/OneStopShopIntegrationTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Org.BouncyCastle.Utilities.Collections;
+
+namespace TestsNUnit;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+internal class OneStopShopIntegrationTests : PageTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task MicrosoftAuthTest()
+    {
+        // Go to Home Page
+        await Page.GotoAsync("http://localhost:5173");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        // Expect a title "to contain" a substring.
+        //await Expect(Page).ToHaveTitleAsync(new Regex("Sign in to your account"));
+
+        // Click Button to register
+        var microsoftAuthPage = await Context.RunAndWaitForPageAsync(async () =>
+        {
+            await Page.ClickAsync(".button");
+        });
+        await microsoftAuthPage.WaitForLoadStateAsync();
+
+        // create a locator
+        await microsoftAuthPage.WaitForTimeoutAsync(2000);
+        var emailInput = await microsoftAuthPage.QuerySelectorAsync("input[type=email]");
+        await emailInput.FillAsync("malisgr@cronos.be");
+
+        var button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
+        await button.ClickAsync();
+
+        // Password page
+        await microsoftAuthPage.WaitForTimeoutAsync(2000);
+        var passwordInput = await microsoftAuthPage.QuerySelectorAsync("input[type=password]");
+        await passwordInput.FillAsync("zaq1@#$ES");
+
+        button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
+        await microsoftAuthPage.WaitForTimeoutAsync(1000);
+        await button.ClickAsync();
+        await microsoftAuthPage.WaitForTimeoutAsync(1000);
+
+        // Microsoft Two step Auth
+        //string divTextContent = await Page.TextContentAsync("#idRichContext_DisplaySign");
+
+        await microsoftAuthPage.WaitForTimeoutAsync(20000);
+
+        // if asks to show message everytime
+        //button = await newPage.QuerySelectorAsync("input[type=submit][value=Tak]");
+        //await button.ClickAsync();
+
+        // Navigate to Reserve Desk Page
+        await Page.GotoAsync("http://localhost:5173/office-details/reserve-desk");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+        await Expect(Page).ToHaveURLAsync("http://localhost:5173/office-details/reserve-desk");
+    }
+}
