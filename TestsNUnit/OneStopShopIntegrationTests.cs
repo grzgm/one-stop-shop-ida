27a560e045cb7b10df0a0ea35720c83e1e8c2e99
Grzegorz <g.malisz@student.fontys.nl>
2024-01-15 08:30:23 +0100

    additional tests and clean up

--- TestsNUnit/OneStopShopIntegrationTests.cs
+++ TestsNUnit/OneStopShopIntegrationTests.cs
@@ -1,8 +1,5 @@
-﻿using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using Org.BouncyCastle.Utilities.Collections;
 
 namespace TestsNUnit;
 
@@ -10,6 +7,9 @@ namespace TestsNUnit;
 [TestFixture]
 internal class OneStopShopIntegrationTests : PageTest
 {
+    private const string p = "zaq1@#$ES";
+    private const string e = "malisgr@cronos.be";
+
     [SetUp]
     public void Setup()
     {
@@ -34,7 +34,7 @@ internal class OneStopShopIntegrationTests : PageTest
         // create a locator
         await microsoftAuthPage.WaitForTimeoutAsync(2000);
         var emailInput = await microsoftAuthPage.QuerySelectorAsync("input[type=email]");
-        await emailInput.FillAsync("malisgr@cronos.be");
+        await emailInput.FillAsync(e);
 
         var button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
         await button.ClickAsync();
@@ -42,7 +42,55 @@ internal class OneStopShopIntegrationTests : PageTest
         // Password page
         await microsoftAuthPage.WaitForTimeoutAsync(2000);
         var passwordInput = await microsoftAuthPage.QuerySelectorAsync("input[type=password]");
-        await passwordInput.FillAsync("zaq1@#$ES");
+        await passwordInput.FillAsync(p);
+
+        button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
+        await microsoftAuthPage.WaitForTimeoutAsync(1000);
+        await button.ClickAsync();
+        await microsoftAuthPage.WaitForTimeoutAsync(1000);
+
+        // Microsoft Two step Auth
+        //string divTextContent = await Page.TextContentAsync("#idRichContext_DisplaySign");
+
+        await microsoftAuthPage.WaitForTimeoutAsync(15000);
+
+        // if asks to show message everytime
+        //button = await newPage.QuerySelectorAsync("input[type=submit][value=Tak]");
+        //await button.ClickAsync();
+
+        // Navigate to Reserve Desk Page
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        await Expect(Page).ToHaveURLAsync("http://localhost:5173/");
+    }
+
+    [Test]
+    public async Task ReserveDeskPageTest()
+    {
+        // Go to Home Page
+        await Page.GotoAsync("http://localhost:5173");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        // Expect a title "to contain" a substring.
+        //await Expect(Page).ToHaveTitleAsync(new Regex("Sign in to your account"));
+
+        // Click Button to register
+        var microsoftAuthPage = await Context.RunAndWaitForPageAsync(async () =>
+        {
+            await Page.ClickAsync(".button");
+        });
+        await microsoftAuthPage.WaitForLoadStateAsync();
+
+        // create a locator
+        await microsoftAuthPage.WaitForTimeoutAsync(2000);
+        var emailInput = await microsoftAuthPage.QuerySelectorAsync("input[type=email]");
+        await emailInput.FillAsync(e);
+
+        var button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
+        await button.ClickAsync();
+
+        // Password page
+        await microsoftAuthPage.WaitForTimeoutAsync(2000);
+        var passwordInput = await microsoftAuthPage.QuerySelectorAsync("input[type=password]");
+        await passwordInput.FillAsync(p);
 
         button = await microsoftAuthPage.QuerySelectorAsync("input[type=submit]");
         await microsoftAuthPage.WaitForTimeoutAsync(1000);
@@ -70,7 +118,7 @@ internal class OneStopShopIntegrationTests : PageTest
         await Page.Locator("input[type=checkbox]").First.ClickAsync();
         // 
         await Page.Locator("button[type=button]").ClickAsync();
-        await Page.WaitForTimeoutAsync(1000); 
+        await Page.WaitForTimeoutAsync(1000);
         // Wait for an alert with class "alert" and text "Successfully Reserved."
         var successAlertElement = await Page.QuerySelectorAsync(".alert:has-text('Successfully Reserved.')");
 
